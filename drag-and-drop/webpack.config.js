6d0bb696e72700f2e29602fc1a6f46bd00947f15
Eunbi <jenabill@naver.com>
2022-12-30 13:22:05 +0900

    🔧 update webpack config

--- drag-and-drop/webpack.config.js
+++ drag-and-drop/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 // nodejs에서 export 하는 방법
 module.exports = {
@@ -8,20 +10,29 @@ module.exports = {
     // 번들된 파일의 위치와 파일명 지정
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: "dist",
   },
   devtool: "inline-source-map",
   module: {
-    // 프로젝트 내에서 다양한 모듈을 처리하는 방법을 지정할 수 있다.
+    // 다양한 유형의 모듈을 처리하는 방법을 지정할 수 있다.
     rules: [
       {
         test: /\.ts$/,
         use: "ts-loader",
         exclude: /node_modules/,
       },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
     ],
   },
   resolve: {
-    extensions: [".ts", ".js"],
+    extensions: [".ts", ".js"], // ts 파일 import 시 확장자 생략 가능
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "public/index.html", // public/index.html 템플릿을 기반으로 빌드 결과물을 추가해줌
+    }),
+    new MiniCssExtractPlugin(), // CSS 파일을 별도로 분리해줌.
+  ],
 };
