0961f42a8d1494b85ca11f8dd4d430a449e0e84a
Eunbi <jenabill@naver.com>
2022-12-30 13:22:26 +0900

    ♻️ import css style file

--- drag-and-drop/src/app.ts
+++ drag-and-drop/src/app.ts
@@ -1,5 +1,6 @@
 import ProjectForm from "./components/project-form";
 import ProjectList from "./components/project-list";
+import "./styles/app.css";
 
 new ProjectForm();
 new ProjectList("active");
